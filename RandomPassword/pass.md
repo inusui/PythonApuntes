dfbb51fc1200707200c5f5e637182c775cc80da6
inusui <radg21@outlook.es>
2022-01-27 14:26:46 -0500

    Se puede emplear el .md y el .txt | Se cuantas lineas he escrito |27-01-22 14:26

---
+++ RandomPassword/pass.md
@@ -0,0 +1,5 @@
+# A ver
+FoWdc<C~&%X4HNht
+GC3VD:qlSgf]7mcA
+4*-E,@O=ftaQ)pBR
+]9e;aM*q(l5_V@U&
--- RandomPassword/randomPassword.py
+++ RandomPassword/randomPassword.py
@@ -4,8 +4,10 @@ lower ="qwertyuiopasdfghjklzxcvbnm"
 mayus = "QWERTYUIOPLKJHGFDSAZXCVBNM"
 numbers = "0123456789"
 symbols = "~!@#$%^&*()_+=/*-|\}{[];:?.>,<"
+#mathSymbols = "⨀⨃⨆⨉⨌⨏⨒⨕⨖⨓⨐⨍⨊⨇⨄⨁⨂⨅⨈⨋⨎⨑⨔⨗⨭⨪⨧⨤⨡⨞⨛⨘⨙⨜⨟⨢⨥⨨⨫⨮⨬⨩⨦⨣⨠⨝⨚⨰⨳⨶⨹⨼⨿⩂⩅⩆⩃⩀⨽⨺⨷⨴⨱⨲⨵⨸⨻⨾⩁⩄⩇⩝⩚⩗⩔⩑⩎⩋⩈⩉⩌⩏⩒⩕⩘⩛⩞⩟⩜⩙⩖⩐⩓⩍⩊⩠⩣⩦⩩⩬⩯⩲⩵"
+#langSymbols ="ΔδηκλΛθΘεβΓζμτπΠΞξΨΩφΦΣχἉἁἙᾁΈέΉἑήΉήἹἱὀΌἺἳὅόῡὛώΏ·ᾤϷϞϘϽϛϠϕϙ"
 
-all = lower + mayus + numbers + symbols
+all = lower + mayus + numbers + symbols #+ mathSymbols + langSymbols
 lenght = 16
 
 password = "".join(random.sample(all, lenght))
@@ -14,6 +16,15 @@ print (password)
 
 #pasalo a un archivo txt por lo menos ¿No?
 
-file1 = open("pass.txt", "a")
+file1 = open("pass.md", "a")
 file1.write("\n"+ password)
 file1.close()
+
+#Leer lineas
+file1 = open ("pass.md", "r")
+lines = file1.readlines()
+print ("Soy la variable lines> " , lines)
+c = 0
+for line in lines:
+    c +=1
+print (c) #imprimo en integer la cantidad de lineas. 

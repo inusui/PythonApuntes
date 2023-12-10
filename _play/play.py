90fd4b4a69340477f6537ecf31fa71028acd0a87
Inusui <radg21@outlook.es>
2023-12-10 14:41:41 -0500

    Un fichero de pruebas y juegos

---
+++ _play/play.py
@@ -0,0 +1,8 @@
+import sentencias
+
+print("""Guenas\n
+      Esto es un programa de pruebas donde creo un archivo, lo importo aca y lo uso
+      solo eso, nada mas que esperabas, es para jugar
+      """)
+
+sentencias
---
+++ _play/sentencias.py
@@ -0,0 +1,23 @@
+print("""Welcome\n
+      Esto es un simple programa que tiene 2 opciones
+      1. Imprime Hola
+      2. una suma toda pendeja
+      3. sale del ciclo while infinito""")
+while(True):
+    print(""" Menu de opciones
+        1) holanda
+        2) sumatoria
+        3) Adios""")
+    opcion = input()
+
+    if opcion == '1':
+        print("esto es un hola")
+    elif opcion == '2':
+        n1 = float(input("Introduce un numero"))
+        n2 = float(input("Introduce otro numero"))
+        print ("El resultado es: ", n1+n2)
+    elif opcion == '3':
+        print("Adios")
+        break
+    else:
+        print("Mal comando we")

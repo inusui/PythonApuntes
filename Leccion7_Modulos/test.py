a189660caf3a5cab104e5b8cd0fceee57464a01b
Inusui <radg21@outlook.es>
2023-12-14 19:50:08 -0500

    Inicio de leccion de Modulos y Paquetes

---
+++ Leccion7_Modulos/test.py
@@ -0,0 +1,15 @@
+# import saludos
+from saludos import saludar, Saludo
+
+saludar()
+Saludo()
+
+
+#Llamar clase
+#Saludo()
+
+""" import sys
+sys.path.insert(1, '..')
+
+from saludos import *
+Saludo() """

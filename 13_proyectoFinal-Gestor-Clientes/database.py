406f28f32c33dd1bfaf5e2396c45d3dd0efd9df9
inusui <radg21@outlook.es>
2025-07-13 16:31:17 -0500

    Implement CSV data handling for Clientes class and unitest

--- 13_proyectoFinal-Gestor-Clientes/database.py
+++ 13_proyectoFinal-Gestor-Clientes/database.py
@@ -1,8 +1,8 @@
 """Controlador de datos
 e interfaz para crear, eliminar y modificar informacion
 """
-
-
+import csv
+import config
 class Cliente:
     def __init__(self, dni, nombre, apellido):
         self.dni = dni
@@ -14,8 +14,14 @@ class Cliente:
 
 
 class Clientes:
-
+    """Clase que maneja la lista de clientes"""
     lista = []
+    with open(config.DATABASE_PATH) as file:
+        reader = csv.reader(file, delimiter=';')
+        for row in reader:
+            dni, nombre, apellido = row
+            lista.append(Cliente(dni, nombre, apellido))
+
 
     @staticmethod
     def buscar(dni):
@@ -28,6 +34,7 @@ class Clientes:
     def crear(dni, nombre, apellido):
         cliente = Cliente(dni, nombre, apellido)
         Clientes.lista.append(cliente)
+        Clientes.guardar()  # Guardar cambios en el archivo CSV
         return cliente
 
     @staticmethod
@@ -37,11 +44,22 @@ class Clientes:
             if cliente.dni == dni:
                 Clientes.lista[indice].nombre = nombre
                 Clientes.lista[indice].apellido = apellido
+                Clientes.guardar()
                 return Clientes.lista[indice]
 
     @staticmethod
     def borrar(dni):
         for indice, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
-                return Clientes.lista.pop(indice)
+                listaCliente = Clientes.lista.pop(indice)
+                Clientes.guardar()
+                return listaCliente
                 
+
+    @staticmethod
+    def guardar():
+        """Guardar la lista de clientes en el archivo CSV"""
+        with open(config.DATABASE_PATH, 'w', newline='\n') as file:
+            writer = csv.writer(file, delimiter=';')
+            for cliente in Clientes.lista:
+                writer.writerow([cliente.dni, cliente.nombre, cliente.apellido])
